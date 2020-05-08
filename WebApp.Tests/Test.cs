32bb19aa18a33b8265dcf8ac20cc24836bdc7a6c
Arunjyoti Dey <61037564+arunjyotidey2705@users.noreply.github.com>
2020-05-08 20:55:26 +1000

    Arun

---
+++ WebApp.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace WebApp.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
